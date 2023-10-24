88a56f05cc572b2e9d2f054647ee86360405d6bc
Sudhansu Kumar Maharana <sudhansumaharana812@gmail.com>
2023-10-24 22:36:07 +0530

    DOM start

--- Section 7 - project/script.js
+++ Section 7 - project/script.js
@@ -1,28 +1,17 @@
 "use strict";
-/*
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".message").textContent = "Correct!!!";
-
-document.querySelector(".score").textContent = 13;
-document.querySelector(".highscore").textContent = 14;
-
-console.log(document.querySelector(".guess").value);
-
-*/
 
 let prizeValue = generateRandomNumber();
+console.log(prizeValue);
 let score = 20;
 let highScore = 0;
-console.log(prizeValue);
 
 function generateRandomNumber() {
 	return Math.floor(Math.random() * 20) + 1;
 }
 
 function clearHouse() {
-	document.querySelector("#alertText").textContent = "";
-	document.querySelector(".guess").value = "";
+	document.querySelector("#alertText").textContent = null;
+	document.querySelector(".guess").value = null;
 	let alertElement = document.getElementById("alertText");
 	alertElement.classList.remove("warning");
 	alertElement.classList.remove("wrongInput");
@@ -32,10 +21,19 @@ function clearHouse() {
 function refreshGame() {
 	clearHouse();
 	prizeValue = generateRandomNumber();
+	console.log(prizeValue);
 	score = 20;
 	document.querySelector(".score").textContent = score;
 	document.getElementById("checkButton").disabled = false;
 	document.getElementById("guessTextBox").disabled = false;
+	document.querySelector("body").style.backgroundColor = "#222";
+}
+
+function resetGame() {
+	highScore = 0;
+	document.querySelector(".highscore").textContent = null;
+	refreshGame();
+	document.querySelector("body").style.backgroundColor = "#222";
 }
 
 function guessNumber(guess) {
@@ -44,28 +42,25 @@ function guessNumber(guess) {
 	if (guess == null || guess == "") {
 		alertElement.classList.add("warning");
 		alertElement.textContent = "Err.. Give me something to work with....";
-	} else if (
-		Number(guess) >= 0 &&
-		Number(guess) <= 20 &&
-		Number(guess) === prizeValue
-	) {
+	} 
+	else if (Number(guess) >= 0 && Number(guess) <= 20 && Number(guess) === prizeValue) {
 		alertElement.classList.add("winnerText");
 		alertElement.textContent = "Fin. Score = " + score;
 		highScore = score > highScore ? score : highScore;
 		score = 20;
 		document.querySelector(".highscore").textContent = highScore;
-		document.querySelector(".score").textContent = highScore;
+		// document.querySelector(".score").textContent = highScore;
 		document.getElementById("checkButton").disabled = true;
 		document.getElementById("guessTextBox").disabled = true;
-	} else if (
-		Number(guess) >= 0 &&
-		Number(guess) <= 20 &&
-		Number(guess) !== prizeValue
-	) {
+		document.querySelector("body").style.backgroundColor = "#60b347";
+		return;
+	} 
+	else if (Number(guess) >= 0 && Number(guess) <= 20 && Number(guess) !== prizeValue) {
 		alertElement.classList.add("warning");
-		alertElement.textContent = "Wrong!! Try Again.";
+		alertElement.textContent = Number(guess) < prizeValue ? "Too low!! Try Again." : "Too high!! Try Again.";
 		score--;
-	} else {
+	} 
+	else {
 		alertElement.classList.add("wrongInput");
 		alertElement.textContent = "Check top right...";
 	}
@@ -78,4 +73,8 @@ document.querySelector(".check").addEventListener("click", function () {
 
 document.querySelector(".again").addEventListener("click", function () {
 	refreshGame();
+});
+
+document.querySelector(".reset").addEventListener("click", function () {
+	resetGame();
 });
